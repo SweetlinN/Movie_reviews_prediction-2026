c92d75af3d57e4db3951f5a375eafd9f0e1cac9a
SweetlinN <sweetlinam621@gmail.com>
2026-03-29 19:48:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import streamlit as st
 import joblib
 
 # Load model
-model = joblib.load('model.joblib')
+import os
+BASE_DIR = os.path.dirname(__file__)
+model_path = os.path.join(BASE_DIR, "model.joblib")
+
+model = joblib.load(model_path)
+model.fit(reviews_train, sent_train)
+vectorizer = joblib.load('vectorizer.joblib')
+
+transformed = vectorizer.transform([user_input])
+prediction = model.predict(transformed)
 
 # Optional: load vectorizer if you used one
 # vectorizer = joblib.load('vectorizer.joblib')
@@ -19,13 +28,6 @@ if st.button("Predict"):
         st.warning("Please enter some text.")
     else:
         try:
-            # If you used vectorizer, uncomment below:
-            # transformed = vectorizer.transform([user_input])
-            # prediction = model.predict(transformed)
-
-            prediction = model.predict([user_input])
-
-            st.success(f"Prediction: {prediction[0]}")
-
+            model = joblib.load("model.joblib")
         except Exception as e:
-            st.error(f"Error: {e}")
+             st.error(f"Model loading failed: {e}")
